1fe1b79f9df1072bf9d736eb429002333f192ceb
Matthew Huus <matthew.huus3@gmail.com>
2020-07-15 02:24:17 -0500

    got the save and view method almost functional, couldn't get ID's.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,41 @@
 // JSON parse
-var noteData =require("../db/db.json");
-const moment = require("moment");
-const { isMoment } = require("moment");
-
+var noteData = require("../db/db.json");
 var data = [];
+id = data
 
-module.exports = function(app){
 
-app.get("/api/notes", function(req,res){
-res.json(noteData);
-})
+module.exports = function (app) {
 
-app.post("/api/notes" , function(req,res){
-   var userData = JSON.parse
-   
-   
-    if(noteData !== ""){
-       noteData.push(req.body);
-       
-        res.json(noteData.concat());
-        noteData.id = isMoment();
+    app.get("/api/notes", function (req, res) {
+        var updatedJson = noteData.concat(req.body);
+
+        res.json(noteData);
+    })
+
+    app.post("/api/notes", function (req, res) {
+        data.push(req.body)
         
-    }
-    else {
-        noteData.push(req.body);
-        res.json(false)
-    }
-})
+        for (let i = 1; i < noteData.length; i++) {
 
-app.post("/api/delete", function(req,res){
+            noteData[i].id = i;
+            
+        }
+        if (noteData !== "") {
 
-})
+
+            noteData.push(req.body);
+            console.log(noteData)
+            res.json(noteData.concat());
+            
+        }
+        else {
+            noteData.push(req.body);
+            res.json(false)
+        }
+    })
+
+    app.delete("/api/delete/:id", function (req, res) {
+        
+    })
 
 }
